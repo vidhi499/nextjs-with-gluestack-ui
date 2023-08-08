c99b3449f6bb463f4201283a300f428af507001c
Vidhi Kataria <vidhikataria@Vidhis-MacBook-Air.local>
2023-08-08 12:50:08 +0530

    ui setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const { withGluestackUI } = require("@gluestack/ui-next-adapter");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  transpilePackages: ["ui-compiled"],
+};
 
-module.exports = nextConfig
+module.exports = withGluestackUI(nextConfig);
